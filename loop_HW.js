8831e147129aae77af60338fc48204f395323de1
Abdullah Alshammari <92294783+ABDULLAHALSHMARI@users.noreply.github.com>
2021-10-17 23:01:06 +0300

    Add files via upload

---
+++ loop_HW.js
@@ -0,0 +1,97 @@
+/*
+//Print every number from 0 to 10
+for(i=0; i<=10; i++ )
+{
+    console.log(i);
+}
+*/
+//Print every number from 10 to 0
+/*
+for(i=10; i>=0; i-- )
+{
+    console.log(i);
+}
+*/
+//Print every number from 4 to -16
+/*
+for(i=4; i>=-16; i-- )
+{
+    console.log(i);
+}
+*/
+//Print every fifth number from 8 to 41
+/*
+for(i=8; i<=41; i+=5){
+    console.log(i)
+}
+*/
+//The classic
+/*
+for(i=1; i<=100; i++){
+    if((i%3==0)&&(i%5==0)){
+        console.log(i+" Fizzbuzz")
+    }
+    else if(i%3==0){
+        console.log(i+" fizz");
+    }
+    else if(i%5==0){
+        console.log(i+" buzz");
+    }
+}
+*/
+//The even/odd reporter
+/*
+for(i=0; i<=20; i++){
+    if(i % 2 == 0){
+        console.log(i+" Even")
+    
+    }
+    else if(i % 2 ==1){
+        console.log(i+ "odd")
+    }
+}
+*/
+//Multiplication table
+/*
+for(let i=0; i<=5; i++){
+    for(let j=1; j<=10; j++)
+    console.log(i+"x"+j+"="+i*j);
+}
+*/
+
+///Fibonacci sequence
+/*
+let a =0;
+let b =1 ;
+let c;
+ for(let i = 1;i <=20; i++){
+            console.log(a);
+    c=a+b;
+    a=b;
+    b=c;
+         }
+*/
+
+//odd
+/*
+let i=1;
+
+while(i<=50){
+    if( i % 2 == 1)
+        console.log(i);
+    
+   
+i++;
+}
+*/
+//Fac
+  /*
+        e=prompt("print a number")
+        let  n=1;
+          while(e>=1){
+          n*=e;
+         e--; 
+       
+    }
+    console.log("Factorial= "+n);
+    */
