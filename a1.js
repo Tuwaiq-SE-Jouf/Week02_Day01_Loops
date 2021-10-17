06d8dfb21bea7393764e307bbccec470c9538a69
Meshal552 <meshl5541@gmail.com>
2021-10-17 21:54:54 +0300

    HMloops

---
+++ a1.js
@@ -0,0 +1,98 @@
+//Print every number from 0 to 10
+
+
+/*for(i=0; i<=10; i++){
+    console.log(i);
+}*/
+
+//Print every number from 10 to 0
+
+
+/*for(i=10; i>=0; i--){
+    console.log(i);}*/
+
+
+   // Print every number from 4 to -16
+
+
+   /* for(i=4; i>=-16;i--){
+        
+        console.log(i);
+        }*/
+
+
+
+   //Print every fifth number from 8 to 41
+
+   /*for(i=8; i <=41; i+=5){
+  console.log(i);
+   }*/
+
+     //The classic Fizzbuzz Program
+
+   /*for(i=1; i <=100; i++){
+       if ((i%3==0) && (i%5==0)){
+           console.log(i+" Fizzbuzz");
+       }
+       else if(i%3==0){
+        console.log(i+ " Fizz")
+    }
+    else if(i%5==0){
+        console.log(i+ " Buzz")
+    }
+    }*/
+
+
+    //The even/odd reporter
+
+    /*for(i=0; i<=20; i++){
+        if(i%2==0){console.log(i+ " Even" )}
+        else if(i%2!==0){console.log(i+ " odd")}
+    }*/
+
+   //Nested for loop: Multiplication table
+
+     /*for(i=1; i<6; i++){
+    for(let j=1; j<11; j++){
+        console.log(i +"*" + j+ "=" + j*i);
+        }
+        }*/
+
+
+        //Fibonacci sequence
+     /* let d =0;
+      let b =1 ;
+      let a;
+      for(let i = 1;i <=20; i++){
+      console.log(d);
+        a=d+b;
+        d=b;
+        b=a;
+         }*/
+
+ 
+
+    
+ 
+ //Odd numbers(while)
+
+ /*let i=1;
+   let number=prompt("Enter the number");
+  while(i<=number){
+      if(number%2!=0)
+      console.log('odd');
+    i++
+  }*/
+
+  //Factorial
+  /*
+   a=prompt('enter number')
+   let Factorial=1;
+   while(a>=1){
+       Factorial*=a;
+       a--; 
+       
+    }
+    console.log('the Factorial= '+Factorial);*/
+   
+
