8f83faf0a695956dd84e0a88424acabd9666da38
amal wash <8amal.8ll@gmail.com>
2021-10-17 18:21:48 +0300

    update fact

---
+++ loops.js
@@ -0,0 +1,9 @@
+let Num = prompt(" Enter the Numbert ")
+
+        let fact = 1;
+        for (i= 1; i <= Num; i++) 
+        {
+
+            fact = fact * i;
+        }
+        console.log("The result of "+ Num +" is "+ fact);
