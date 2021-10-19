a2be85eab7039ab3b3a82c94789dd4c99cdb9d99
amal wash <8amal.8ll@gmail.com>
2021-10-19 09:44:19 +0300

    function bonus

--- lo.js
+++ lo.js
@@ -1,5 +1,6 @@
- /*
+ 
  //The even/odd reporter
+ function chec(){
  let i=1;
 let nub = prompt('Enter number');
 
@@ -7,37 +8,55 @@ while(i <= nub){
     if(i%2!=0)
     console.log(i);
     i++;
-} 
+}
+}
+chec() ;
 
 
-/*
+
 //For loop
+function loops(){
 let i =0;
 while(i<=10){
     console.log(i);
     i++;
 }
+}
+loops();
 
+//
 let i =10;
+function loop(){
 while(i>=0){
     console.log(i);
     i--;
 }
+}
+loop();
+//
 
+function add(){
 let i = 4;
 while(i >= -16){
     console.log(i);
     i--;  
 }
+}
+add();
+//
 
 let i =8;
+function ask(){
 while(i <=41){
     console.log(i);
     i +=5;
 }
+}
+ask();
  //The classic Fizzbuzz Program
 
 let i=1;
+function fizzbuzz(){
 while(i<=100){
     if(i %3 ==0 && i %5 ==0){
         console.log(i + "fizzbuzz");
@@ -50,22 +69,28 @@ while(i<=100){
     }
     i++;
 }
+}
+fizzbuzz();
 
 //Nested for loop
 
+function Nested(){
 
 for (let i=1 ; i<=5; i++){
     for(let j=1; j<=10;j++){
         console.log(i+ '*' + j + '=' + i * j );
     }
 }
-*/
+}
+Nested();
+
 //Fibonacci sequence
-/*
+
+
 let i1 = 0
 let i2 = 1
 let nextn=i2 ;
-
+function  sequence(){
     for (let m = 0; m <= 20; m++) 
     {
     console.log(nextn);
@@ -78,26 +103,35 @@ let nextn=i2 ;
    
   
   }
-  
+
+sequence();
 
 //Odd numbers
+function odd(){
 let n = prompt("Enter the Number");
 let nu = 1;
+
 while(nu <= n){
 if(nu %2 !== 0)
 console.log(nu);
   nu++;
 }
-*/
+}
+odd();
+
 
 //Factorial
+
 
 let Num = prompt(" Enter the Numbert ")
 
         let fact = 1;
+        function Factorial(){
         for (i= 1; i <= Num; i++) 
         {
 
             fact = fact * i;
         }
         console.log("The result of "+ Num +" is "+ fact);
+    }
+    Factorial();
